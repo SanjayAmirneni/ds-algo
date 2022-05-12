cea2479c2edea481a5964d064be576a8e0041743
SanjayAmirneni <sanjayamirneni@gmail.com>
2022-05-12 14:51:39 -0500

    order added

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,16 @@
-function bubblesort(arr){
-    let noSwaps = true
+function bubblesort(arr,order){
+    let noSwaps = true;
+    let flag = false;
+    console.log(typeof(order), order)
     for(let i=arr.length; i>0; i--){
         noSwaps = true;
         for(let j=0; j<i-1; j++){
-            if(arr[j] > arr[j+1]){
+            if(order === 'asc'){
+                flag = arr[j] > arr[j+1]
+            }else if(order === 'dsc'){
+                flag = arr[j] < arr[j-1]
+            }
+            if(flag){
                 let temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
@@ -17,5 +24,5 @@ function bubblesort(arr){
 }
 
 
-console.log(bubblesort([9,1,2,3,4,5,6,7,8]))
+console.log(bubblesort([9,1,2,3,4,5,6,7,8],'dsc'))
 
